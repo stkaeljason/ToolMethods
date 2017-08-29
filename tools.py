3d4e79810734af9534ab025ee68dab76ac57d0a6
jason <pudge702@qq.com>
2017-08-29 16:35:47 +0800

    init git

---
+++ tools.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+from collections import defaultdict, Counter
+
+
+def my_count1(seq):
+    """计算序列中元素出现的次数，并排序返回，seq为可迭代对象"""
+    seq_dict = defaultdict(lambda: 0)
+    for s in seq:
+        seq_dict[s] += 1
+    return sorted(seq_dict.items(), key=lambda x: x[1])
+
+
+def my_count2(seq):
+    """计算序列中元素出现的次数，并排序返回，seq为可迭代对象,使用Counter"""
+    return sorted(Counter(seq).items(), key=lambda x: x[1])
